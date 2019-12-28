ff9b8bf83994cd72827e0b85a19b5c6dabae07f2
HarryPyc <yucongpan@qq.com>
2019-12-28 15:15:54 -0600

    Optimize Mesh Objects

---
+++ Raytracing/Assets/Scripts/RayTracingObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class RayTracingObject : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        RayTracingMaster.RegisterObject(this);
+    }
+    private void OnDisable()
+    {
+        RayTracingMaster.UnregisterObject(this);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
